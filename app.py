dbbf67b9101716a114be27f0d5804048ac1dcebf
sbonala3 <sbonala3@gatech.edu>
2018-04-22 15:11:58 -0400

    Removed unnecessary socketio functions from app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def test_disconnect():
 
 
 @socketio.on('check_db', namespace='/test')
-def check_db(message):
+def check_db():
     global dbcount, socketio
     conn = MySQLdb.connect (host = HOST,
                             user = USER,
@@ -210,73 +210,5 @@ if __name__ == '__main__':
     #!app.run(debug=True, port=5000)
     socketio.run(app, debug=True)
 
-
-#! Socketsss
-# @socketio.on('my_event', namespace='/test')
-# def test_message(message):
-#     print("Got my event, next should go to my response")
-#     # session['receive_count'] = session.get('receive_count', 0) + 1
-#     # emit('my_response',
-#     #      {'data': message['data'], 'count': session['receive_count']})
-
-# @socketio.on('my_broadcast_event', namespace='/test')
-# def test_broadcast_message(message):
-#     global count
-#     count = count + 1
-#     print(count)
-    # session['receive_count'] = session.get('receive_count', 0) + 1
-    # emit('my_response',
-    #      {'data': message['data'], 'count': session['receive_count']},
-    #      broadcast=True)
-
-
-# @socketio.on('join', namespace='/test')
-# def join(message):
-#     join_room(message['room'])
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     emit('my_response',
-#          {'data': 'In rooms: ' + ', '.join(rooms()),
-#           'count': session['receive_count']})
-
-
-# @socketio.on('leave', namespace='/test')
-# def leave(message):
-#     leave_room(message['room'])
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     emit('my_response',
-#          {'data': 'In rooms: ' + ', '.join(rooms()),
-#           'count': session['receive_count']})
-
-
-# @socketio.on('close_room', namespace='/test')
-# def close(message):
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-#                          'count': session['receive_count']},
-#          room=message['room'])
-#     close_room(message['room'])
-
-
-# @socketio.on('my_room_event', namespace='/test')
-# def send_room_message(message):
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     emit('my_response',
-#          {'data': message['data'], 'count': session['receive_count']},
-#          room=message['room'])
-
-
-# @socketio.on('disconnect_request', namespace='/test')
-# def disconnect_request():
-#     global thread
-#     print("Disconnect message received!")
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     emit('my_response',
-#          {'data': 'Disconnected!', 'count': session['receive_count']})
-#     #disconnect()
-#     #if thread.is_alive():
-#     #thread.terminate()
-#     #thread.kill()
-#     print("Finished disconnect")
-#     thread = None
 
 
